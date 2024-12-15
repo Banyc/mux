8b5f29297db24f9f6caff7c15e6f76094cfa5bc0
Banyc <36535895+Banyc@users.noreply.github.com>
2024-12-15 20:43:18 +0800

    fix: public API

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ mod protocol;
 mod serve;
 mod stream;
 
-pub use control::Initiation;
+pub use control::{DeadControl, Initiation};
 pub use serve::{spawn_mux_no_reconnection, spawn_mux_with_reconnection, MuxConfig, MuxError};
-pub use stream::{accepter::StreamAccepter, reader::StreamReader, writer::StreamWriter};
+pub use stream::{
+    accepter::StreamAccepter,
+    opener::{StreamOpenError, StreamOpener},
+    reader::StreamReader,
+    writer::StreamWriter,
+};
